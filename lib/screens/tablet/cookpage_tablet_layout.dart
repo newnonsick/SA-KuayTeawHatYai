62f2dcd3a3f40982b3ad9032adb75b6116fc901b
Nattapong1538 <nattapong.lep@ku.th>
2024-10-11 22:10:49 +0700

    Add cookpage with Navigation Rail

--- lib/screens/tablet/cookpage_tablet_layout.dart
+++ lib/screens/tablet/cookpage_tablet_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CookPageTabletLayout extends StatefulWidget {
   const CookPageTabletLayout({super.key});
@@ -8,8 +9,155 @@ class CookPageTabletLayout extends StatefulWidget {
 }
 
 class _CookPageTabletLayoutState extends State<CookPageTabletLayout> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // return ResponsiveLayout.isPortrait(context)
+    //     ? const PortraitModePage()
+    //     : _buildLandScape();
+    return _buildLandScape();
+  }
+
+  Widget _buildLandScape() {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Row(
+          children: [
+            _buildCustomNavigationRail(),
+            Expanded(
+              child: IndexedStack(
+                index: _selectedIndex,
+                children: [
+                  Container(
+                    color: Colors.white,
+                    child: const Center(
+                      child: Text(
+                        'ออเดอร์',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    child: const Center(
+                      child: Text(
+                        'จัดการวัตถุดิบ',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCustomNavigationRail() {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          right: BorderSide(
+            color: Colors.grey[400]!,
+            width: 1,
+          ),
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 10),
+            _buildCustomNavigationRailItem(
+              icon: Icons.article,
+              label: 'ออเดอร์',
+              index: 0,
+            ),
+            _buildCustomNavigationRailItem(
+              icon: Icons.edit,
+              label: 'จัดการวัตถุดิบ',
+              index: 1,
+            ),
+            _buildCustomNavigationRailItem(
+              icon: Icons.arrow_back_ios_new,
+              label: 'กลับ',
+              index: -1,
+              onTap: (_) => Get.back(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCustomNavigationRailItem({
+    required IconData icon,
+    required String label,
+    required int index,
+    ValueChanged<void>? onTap,
+  }) {
+    final isSelected = _selectedIndex == index;
+
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap(null);
+        } else {
+          setState(() {
+            _selectedIndex = index;
+          });
+        }
+      },
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: isSelected ? const Color(0xFFF8C324) : Colors.grey[300],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                  child: Icon(
+                icon,
+                color: Colors.black,
+                size: 25,
+              )),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              label,
+              style: const TextStyle(
+                fontSize: 11,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 15),
+            if (index != -1)
+              Container(
+                height: 2,
+                width: 20,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )
+          ],
+        ),
+      ),
+    );
   }
 }
